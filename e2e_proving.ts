689c80d93881e147f0648984e3c316b5b77669a9
sulejman <3107367+Sulejman@users.noreply.github.com>
2023-12-19 17:15:56 +0100

    Updates for larger number of addresses

--- e2e_proving.ts
+++ e2e_proving.ts
@@ -7,23 +7,31 @@ import {
   VerifyConfig
 } from "@personaelabs/spartan-ecdsa";
 import { hashPersonalMessage, privateToAddress, ecsign, toRpcSig } from "@ethereumjs/util";
-
-
-
+import chalk from "chalk";
+import figlet from "figlet";
 const hushProofProverConfigDefault: ProverConfig = {
-  witnessGenWasm:
-    "./addr_membership.wasm",
-  circuit:
-    "./addr_membership.circuit"
+  witnessGenWasm: "./addr_membership.wasm",
+  circuit: "./addr_membership.circuit"
 };
 
 const hushProofVerifierConfigDefault: VerifyConfig = {
   circuit: hushProofProverConfigDefault.circuit
 };
 
+const printHeader = (text: string) => {
+  console.log(chalk.bold.blue(figlet.textSync(text)));
+};
+
+const printStep = (text: string) => {
+  console.log(chalk.yellow(`\n${text}\n`));
+};
+
+const printDataStructure = (text: string) => {
+  console.log(chalk.green(`\n${text}\n`));
+};
 
 const proveMembership = async () => {
-
+  printHeader("Proving Membership");
 
   // Init the Poseidon hash
   const poseidon = new Poseidon();
@@ -41,40 +49,98 @@ const proveMembership = async () => {
   // Insert prover public key hash into the tree
   tree.insert(proverAddress);
 
+  printStep("Inserting Prover Public Key Hash into the Tree");
+  printDataStructure(tree.toString());
 
-  // Insert other members into the tree
-  for (const member of ["🕵️", "🥷", "👩‍🔬"]) {
-    tree.insert(
-      BigInt(
-        "0x" + Buffer.from("".padStart(16, member), "utf16le").toString("hex")
-      )
-    );
+  // // Insert other members into the tree
+  // for (const member of ["🕵️", "🥷", "👩‍🔬"]) {
+  //   tree.insert(
+  //     BigInt(
+  //       "0x" + Buffer.from("".padStart(16, member), "utf16le").toString("hex")
+  //     )
+  //   );
+  // }
+
+  // printStep("Inserting Other Members into the Tree");
+  // printDataStructure(tree.toString());
+
+  // Generate 32,000 private keys and addresses
+  const privKeys = Array.from({ length: 1023 }, (_, i) => {
+    const privKey = Buffer.from(i.toString().padStart(16, "0"), "utf16le");
+    const address = privateToAddress(privKey);
+    console.log(chalk.yellow(`Generated address: ${"0x" + Buffer.from(address).toString("hex")}`));
+    return privKey;
+  });
+
+  // Store addresses hashes
+  const addresses: bigint[] = [];
+
+  // Compute public key hashes
+  for (const privKey of privKeys) {
+    const address = privateToAddress(privKey);
+    addresses.push(BigInt("0x" + Buffer.from(address).toString("hex")));
   }
 
-  // Compute the merkle proof
-  const index = tree.indexOf(proverAddress);
-  const merkleProof = tree.createProof(index);
+  const progressBarDraw = (current: number, total: number) => {
+    const percentage = Math.floor((current / total) * 100);
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+    const progressBar = (current: number, total: number) => {
+      const percentage = Math.floor((current / total) * 100);
+      const filledLength = Math.floor(percentage / 10);
+      const bar = '='.repeat(filledLength) + ' '.repeat(10 - filledLength);
+      process.stdout.write(`Progress: [${bar}] ${percentage}%\r`);
+    };
+  }
 
-  // Init the prover
-  const prover = new MembershipProver(hushProofProverConfigDefault);
-  await prover.initWasm();
+    // Insert the pubkey hashes into the tree
+    for (let i = 0; i < addresses.length; i++) {
+      tree.insert(addresses[i]);
+      progressBarDraw(i + 1, addresses.length);
+    }
 
-  const msgHash = hashPersonalMessage(Buffer.from("harry potter"));
+    printStep("Inserting Public Key Hashes into the Tree");
+    printDataStructure(tree.toString());
 
-  const { v, r, s } = ecsign(msgHash, privKey);
+    console.log(chalk.cyan("Sanity check (check that there are not duplicate members)"));
+    console.log(chalk.cyan(new Set(addresses).size === addresses.length));
 
-  // Convert to RPC signature format
-  const sig = toRpcSig(v, r, s);
+    // Compute the merkle proof
+    const index = tree.indexOf(proverAddress);
+    const merkleProof = tree.createProof(index);
 
-  // Prove membership
-  const { proof, publicInput } = await prover.prove(sig, Buffer.from(msgHash), merkleProof);
+    printStep("Computing Merkle Proof");
+    console.log(chalk.cyan("Merkle Proof:"));
+    console.log(merkleProof);
 
-  // Init verifier
-  const verifier = new MembershipVerifier(hushProofVerifierConfigDefault);
-  await verifier.initWasm();
+    // Init the prover
+    const prover = new MembershipProver(hushProofProverConfigDefault);
+    await prover.initWasm();
 
-  // Verify proof
-  await verifier.verify(proof, publicInput.serialize());
-};
+    const msgHash = hashPersonalMessage(Buffer.from("harry potter"));
 
-proveMembership().then(() => console.log("done"));
+    const { v, r, s } = ecsign(msgHash, privKey);
+
+    // Convert to RPC signature format
+    const sig = toRpcSig(v, r, s);
+
+    // Prove membership
+    const { proof, publicInput } = await prover.prove(sig, Buffer.from(msgHash), merkleProof);
+
+    printStep("Proving Membership");
+    console.log(chalk.cyan("Proof:"));
+    console.log(proof);
+    console.log(chalk.cyan("Public Input:"));
+    console.log(publicInput.serialize());
+
+    // Init verifier
+    const verifier = new MembershipVerifier(hushProofVerifierConfigDefault);
+    await verifier.initWasm();
+
+    // Verify proof
+    await verifier.verify(proof, publicInput.serialize());
+
+    console.log(chalk.green("\nDone"));
+  };
+
+  proveMembership().catch((error) => console.error(chalk.red(error)));
