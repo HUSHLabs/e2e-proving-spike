25f0633a7fc4442d11891e8586068ca161718a9a
sulejman <3107367+Sulejman@users.noreply.github.com>
2023-12-19 23:06:13 +0100

    Updated demo

--- e2e_proving.ts
+++ e2e_proving.ts
@@ -31,7 +31,7 @@ const printDataStructure = (text: string) => {
 };
 
 const proveMembership = async () => {
-  printHeader("Proving Membership");
+  printHeader("HushProof PoC");
 
   // Init the Poseidon hash
   const poseidon = new Poseidon();
@@ -48,27 +48,15 @@ const proveMembership = async () => {
 
   // Insert prover public key hash into the tree
   tree.insert(proverAddress);
-
   printStep("Inserting Prover Public Key Hash into the Tree");
-  printDataStructure(tree.toString());
 
-  // // Insert other members into the tree
-  // for (const member of ["🕵️", "🥷", "👩‍🔬"]) {
-  //   tree.insert(
-  //     BigInt(
-  //       "0x" + Buffer.from("".padStart(16, member), "utf16le").toString("hex")
-  //     )
-  //   );
-  // }
+  printStep("Generating 4096 Ethereum Addresses, with pool of addresses this large it should be infeasible to find out which address is the prover's address.");
 
-  // printStep("Inserting Other Members into the Tree");
-  // printDataStructure(tree.toString());
-
-  // Generate 32,000 private keys and addresses
-  const privKeys = Array.from({ length: 1023 }, (_, i) => {
+  // Generate private keys and addresses
+  const privKeys = Array.from({ length: 4095 }, (_, i) => {
     const privKey = Buffer.from(i.toString().padStart(16, "0"), "utf16le");
     const address = privateToAddress(privKey);
-    console.log(chalk.yellow(`Generated address: ${"0x" + Buffer.from(address).toString("hex")}`));
+    process.stdout.write(`Generated address ${i + 1}: ${chalk.green(`${"0x" + Buffer.from(address).toString("hex")}\r`)}`);
     return privKey;
   });
 
@@ -80,67 +68,65 @@ const proveMembership = async () => {
     const address = privateToAddress(privKey);
     addresses.push(BigInt("0x" + Buffer.from(address).toString("hex")));
   }
-
-  const progressBarDraw = (current: number, total: number) => {
+  
+  const progressBar = (current: number, total: number, address: string) => {
     const percentage = Math.floor((current / total) * 100);
-    process.stdout.clearLine(0);
-    process.stdout.cursorTo(0);
-    const progressBar = (current: number, total: number) => {
-      const percentage = Math.floor((current / total) * 100);
-      const filledLength = Math.floor(percentage / 10);
-      const bar = '='.repeat(filledLength) + ' '.repeat(10 - filledLength);
-      process.stdout.write(`Progress: [${bar}] ${percentage}%\r`);
-    };
-  }
-
-    // Insert the pubkey hashes into the tree
-    for (let i = 0; i < addresses.length; i++) {
-      tree.insert(addresses[i]);
-      progressBarDraw(i + 1, addresses.length);
-    }
-
-    printStep("Inserting Public Key Hashes into the Tree");
-    printDataStructure(tree.toString());
-
-    console.log(chalk.cyan("Sanity check (check that there are not duplicate members)"));
-    console.log(chalk.cyan(new Set(addresses).size === addresses.length));
-
-    // Compute the merkle proof
-    const index = tree.indexOf(proverAddress);
-    const merkleProof = tree.createProof(index);
-
-    printStep("Computing Merkle Proof");
-    console.log(chalk.cyan("Merkle Proof:"));
-    console.log(merkleProof);
-
-    // Init the prover
-    const prover = new MembershipProver(hushProofProverConfigDefault);
-    await prover.initWasm();
-
-    const msgHash = hashPersonalMessage(Buffer.from("harry potter"));
-
-    const { v, r, s } = ecsign(msgHash, privKey);
-
-    // Convert to RPC signature format
-    const sig = toRpcSig(v, r, s);
-
-    // Prove membership
-    const { proof, publicInput } = await prover.prove(sig, Buffer.from(msgHash), merkleProof);
-
-    printStep("Proving Membership");
-    console.log(chalk.cyan("Proof:"));
-    console.log(proof);
-    console.log(chalk.cyan("Public Input:"));
-    console.log(publicInput.serialize());
-
-    // Init verifier
-    const verifier = new MembershipVerifier(hushProofVerifierConfigDefault);
-    await verifier.initWasm();
-
-    // Verify proof
-    await verifier.verify(proof, publicInput.serialize());
-
-    console.log(chalk.green("\nDone"));
+    process.stdout.write(`\rInserting address ${current + 1} to tree: [${'='.repeat(percentage / 5)}${' '.repeat(20 - percentage / 5)}] ${percentage}% - Ethereum Address: ${chalk.green(address)}`);
   };
 
-  proveMembership().catch((error) => console.error(chalk.red(error)));
+  const insertPubKeyHashes = () => {
+    for (let i = 0; i < addresses.length; i++) {
+      const address = addresses[i];
+      tree.insert(address);
+      progressBar(i + 1, addresses.length, "0x" + address.toString(16));
+    }
+  };
+
+  await insertPubKeyHashes();
+  console.log("\n")
+
+  console.log(chalk.cyan("Sanity check (check that there are not duplicate members)"));
+  console.log(chalk.cyan(new Set(addresses).size === addresses.length));
+
+  // Compute the merkle proof
+  const index = tree.indexOf(proverAddress);
+  const merkleProof = tree.createProof(index);
+
+  printStep("Computing Merkle Proof");
+  // console.log(chalk.cyan("Merkle Proof:"));
+  // console.log(merkleProof);
+
+  // Init the prover
+  const prover = new MembershipProver(hushProofProverConfigDefault);
+  await prover.initWasm();
+
+  printStep("Generating Signature, prover now signs a message with his private key and generates a signature.");
+
+  const msgHash = hashPersonalMessage(Buffer.from("harry potter"));
+
+  const { v, r, s } = ecsign(msgHash, privKey);
+
+  // Convert to RPC signature format
+  const sig = toRpcSig(v, r, s);
+
+  // Prove membership
+  const { proof, publicInput } = await prover.prove(sig, Buffer.from(msgHash), merkleProof);
+
+  printStep("Proving Membership, prover now proves that he is a member of the subset of addresses and has signed the message.");
+
+  console.log(chalk.cyan("Proof:"));
+  console.log(proof);
+  console.log(chalk.cyan("Public Input:"));
+  console.log(publicInput.serialize());
+
+  // Init verifier
+  const verifier = new MembershipVerifier(hushProofVerifierConfigDefault);
+  await verifier.initWasm();
+
+  // Verify proof
+  await verifier.verify(proof, publicInput.serialize());
+
+  console.log(chalk.green("\nDone"));
+};
+
+proveMembership().catch((error) => console.error(chalk.red(error)));
